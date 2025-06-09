372c1ef85231dc1b01008d58e6dfe81b392565f3
zanghaei <69038229+zanghaei@users.noreply.github.com>
2025-06-09 15:32:41 +0330

    Update ExampleNDA1.m

--- ExampleNDA1.m
+++ ExampleNDA1.m
@@ -10,7 +10,6 @@ for i=1:10
         'Please Waite','ForceSameSize','off','VarNames',{'Y'},'NTry',1,'CutStatus','off',0,'FunctionalizingMethod','variable','Type',0);
     NoiseLevel(i,1)=i;
     NDA_Result(i,1)=NDAResult;
-    plot
 end
 T = table(NoiseLevel,NDA_Result)
 disp('if you are satistified please run ExampleArtileFigures_Linear.m or  ExampleArtileFigures_Sin  or ExampleArtileFigures_DiscreteSine')
